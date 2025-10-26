9ac9e234e7d16cdd63cf339a152a24db20d7aeba
tejas <tejasdeshmukh320@gmail.com>
2025-10-27 01:01:38 +0530

    Changes done

--- app.js
+++ app.js
@@ -191,8 +191,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Multer setup (in-memory)
 const storage = multer.memoryStorage();
